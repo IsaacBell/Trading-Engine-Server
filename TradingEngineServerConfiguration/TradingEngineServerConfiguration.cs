c95427b53c5f36d9ef31b8463e810a7a88caed72
Isaac Bell <isaacbell388@gmail.com>
2022-12-27 15:16:34 -0800

    Add project files.

---
+++ TradingEngineServerConfiguration/TradingEngineServerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Core.Configuration
+{
+    class TradingEngineServerConfiguration
+    {
+    }
+
+    class TradingEngineServerSettings
+    {
+        public int Port { get; set; }
+    }
+}
---
+++ TradingEngineServerHostBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingEngineServer.Core.Configuration;
+
+namespace TradingEngineServer.Core
+{
+    public sealed class TradingEngineServerHostBuilder
+    {
+        public static IHost BuildTradingEngineServer() => Host.CreateDefaultBuilder().ConfigureServices((ctx, services) => {
+            services.AddOptions();
+            services.Configure<TradingEngineServerConfiguration>(ctx.Configuration.GetSection(nameof(TradingEngineServerConfiguration)));
+
+            services.AddSingleton<ITradingEngineServer, TradingEngineServer>();
+
+            services.AddHostedService<TradingEngineServer>();
+        }).Build();
+    }
+}
